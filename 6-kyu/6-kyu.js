5b0e98687088570e5a00dcdbcab0e9712972173c
Isabel González <isaabelglez@gmail.com>
2023-06-01 21:08:13 +0100

    kyu 6

--- 6-kyu/6-kyu.js
+++ 6-kyu/6-kyu.js
@@ -1,11 +1,51 @@
+function getHeightByPokemonId(id) {
+
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+    return fetch(url)
+      .then( response => response.json())
+      .then( pokemon => {
+  
+        return pokemon.height;
+      });
+  }
 
 
 
+function getPokemonsByType(type){
+const url = `https://pokeapi.co/api/v2/type/${type}`
+
+return fetch(url)
+.then( response => response.json())
+.then( pokemon => {
+  return pokemon;
+})
+}
+
+function getPokemonSpeciesNameByGeneration(generation) {
+
+  const url = `https://pokeapi.co/api/v2/generation/${generation}`;
+
+  return fetch(url)
+    .then((response) => response.json())
+
+    .then((generation) => getPokemonSpeciesNames(generation.pokemon_species));
+
+}
+
+function getPokemonSpeciesNames(pokemonSpecies) {
+
+  const pokemonSpeciesNames = [];
+
+  pokemonSpecies.forEach((pokemon) => {
+
+    pokemonSpeciesNames.push(pokemon.name);
+
+  });
+  return pokemonSpeciesNames;
+}
 
 
 
-
-
-
-
-module.exports = {getNoldorByMurdered,}
+module.exports = {getHeightByPokemonId,getPokemonsByType,
+  getPokemonSpeciesNameByGeneration, getHeightsByType,}
